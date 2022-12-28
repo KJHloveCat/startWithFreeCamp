cd14b24cc890de41d741d5855bb49c55cb9d0096
KJHloveCat <kjh99407@gmail.com>
2022-12-28 18:18:03 +0900

    1228

--- class/03-javascript/04-document.js
+++ class/03-javascript/04-document.js
@@ -7,3 +7,11 @@ function greeting() {
 
 
 }
+
+function send() {
+
+    let tokenNum = String(Math.floor( Math.random() * 1000000 )).padStart(6, "0")
+
+    document.getElementById("token").innerText = tokenNum
+    
+}
