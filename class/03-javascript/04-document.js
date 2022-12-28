a9bad256714fdc163cc376c13f02c4de8bd75bd2
KJHloveCat <kjh99407@gmail.com>
2022-12-28 16:57:55 +0900

    v4

---
+++ class/03-javascript/04-document.js
@@ -0,0 +1,9 @@
+function greeting() {
+
+
+    document.getElementById("target").innerText = document.getElementById("input").value
+
+    // document.getElementById("target").innerText = "World"
+
+
+}
